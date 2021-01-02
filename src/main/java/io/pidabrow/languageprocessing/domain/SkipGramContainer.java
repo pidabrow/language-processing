3f4a489dc98809215cfb715ae70f263085675987
Piotr Dąbrowski <piotr.b.dabrowski@gmail.com>
2021-01-02 16:29:13 +0100

    Work in progress (logging).

--- src/main/java/io/pidabrow/languageprocessing/domain/SkipGramContainer.java
+++ src/main/java/io/pidabrow/languageprocessing/domain/SkipGramContainer.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,11 +26,24 @@ public class SkipGramContainer {
     }
 
     private List<Integer> analyseGapSizes() {
-        return List.of(0);
+        int[] tokenIds = tokens.stream()
+                .map(Token::getTokenId)
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        List<Integer> gapSizes = new ArrayList<>();
+
+        for(int i = 0; i < tokenIds.length - 1; i++) {
+            int distance = tokenIds[i+1] - tokenIds[i] - 1; // extra -1 is because tokenIds[3] - tokenIds[1] must return 1, not 2
+            gapSizes.add(distance);
+        }
+
+        return gapSizes;
     }
 
     @Override
     public String toString() {
-        return String.join(" ", tokens.stream().map(Token::getValue).collect(Collectors.toList()));
+        return String.join(" ", tokens.stream().map(Token::getValue).collect(Collectors.toList()))
+                + (gapSizes.isEmpty() ? "" : " (gaps: " + gapSizes.stream().map(String::valueOf).collect(Collectors.joining(",")) + ")");
     }
 }
